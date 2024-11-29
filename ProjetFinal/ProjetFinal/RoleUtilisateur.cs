e52513b2c24d137d379b9c32c88a90818a70e16f
tonysk8126 <anthosk8@outlook.fr>
2024-11-29 09:40:58 -0500

    RoleUtilisateur Créer

---
+++ ProjetFinal/ProjetFinal/RoleUtilisateur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinal
+{
+    internal static class RoleUtilisateur
+    {
+		private static bool admin;
+
+		public static bool Admin
+		{
+			get { return admin; }
+			set { admin = value; }
+		}
+
+		private static bool utilisateurConnecte;
+
+		public static bool UtilisateurConnecte
+		{
+			get { return utilisateurConnecte; }
+			set { utilisateurConnecte = value; }
+		}
+	}
+}
